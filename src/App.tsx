0f5e34211a2b63feb8a3c9861f61f1cd01c56ea5
LinusLM <linu0931@frbskole.dk>
2024-08-13 13:16:23 +0200

    hej

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   let [count, setCount] = useState(0);
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <>
+      <Analytics>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -45,6 +47,7 @@ function App() {
         </p>
       </div>
       <div className="countis100" ref={messageRef}></div>
+      </Analytics>
     </>
   );
 }
